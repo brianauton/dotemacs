55d85631f5f37999fcf4a67c4f1241852379b2fd
Brian Auton <brianauton@gmail.com>
2015-01-04 12:26:05 -0500

    Add ws-trim to delete trailing whitespace from all edited lines

--- init.el
+++ init.el
@@ -5,6 +5,11 @@
 (add-to-list 'load-path "~/.emacs.d/tomorrow-theme")
 (require 'tomorrow-night-bright-theme)
 
+(add-to-list 'load-path "~/.emacs.d/ws-trim")
+(require 'ws-trim)
+(global-ws-trim-mode t)
+(set-default 'ws-trim-level 0)
+
 (setq
  indent-tabs-mode nil
  kill-whole-line t
