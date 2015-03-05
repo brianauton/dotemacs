a2dde08767f1f5272b032e125be773699660bba5
Brian Auton <brianauton@gmail.com>
2015-03-04 19:19:58 -0500

    Automatically create directories when editing a new file

--- init.el
+++ init.el
@@ -31,3 +31,10 @@
  )
 
 (menu-bar-mode -1)
+
+(defadvice find-file (before make-directory-maybe (filename &optional wildcards) activate)
+  "Create parent directory if not exists while visiting file."
+  (unless (file-exists-p filename)
+    (let ((dir (file-name-directory filename)))
+      (unless (file-exists-p dir)
+	(make-directory dir t)))))
